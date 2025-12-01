a4f53ccac34f7346a3b37cc3c01a6d6c010bcda5
Mohamedlutfi <momo24wv@student.ju.se>
2025-12-01 22:15:05 +0100

    all game activitys/fragments

---
+++ app/src/main/java/se/jumomo24wv/menuactivity/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package se.jumomo24wv.menuactivity
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
