6cd5f8140bd8d426888ae78ecf2999c49c85dd58
Diab-11 <dizk24wt@student.ju.se>
2025-12-01 21:53:51 +0100

    Create game layout

--- app/src/main/java/se/jumomo24wv/menuactivity/CreateGame.kt
+++ app/src/main/java/se/jumomo24wv/menuactivity/CreateGame.kt
@@ -1,40 +1,20 @@
 package se.jumomo24wv.menuactivity
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import se.jumomo24wv.menuactivity.databinding.ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class CreateGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_create_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        with(binding){
-            menuStartGame.setOnClickListener {
-                createGameFun()
-            }
-
-        }
-
     }
-    fun createGameFun(){
-        intent = Intent(this, CreateGame::class.java)
-        startActivity(intent)
-    }
-
 }
